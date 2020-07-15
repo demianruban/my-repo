29ec524a46d41a77ceeab799b13c49beec847f8c
Demian <48176400+demianloveass@users.noreply.github.com>
2020-07-15 15:13:19 +0300

    added new program
    Script that sorts files under path where it located.

---
+++ sorter.py
@@ -0,0 +1,38 @@
+import os
+
+location = os.getcwd()
+
+print("Current location:", location)
+
+main_dict = {
+    'Executables': '.exe',
+    'Documents'  : ('.pdf', '.txt', '.iso'),
+    'Images'     : ('.png', '.jpeg', '.gif'),
+    'Archives'   : ('.zip', '.rar', '.7z')
+    }
+
+def main():
+    
+    for folder_name in main_dict:
+
+        os.mkdir(folder_name)
+        
+        if type(main_dict[folder_name]) is tuple:
+            for file_format in main_dict[folder_name]:
+                for file_name in os.listdir():    
+                    if file_name.endswith(file_format):
+                        os.replace(os.getcwd() + '\\' + file_name,
+                                   os.getcwd() + '\\' + folder_name
+                                   + '\\' + file_name)
+
+        else:
+            file_format = main_dict[folder_name]
+            for file_name in os.listdir():
+                if file_name.endswith(file_format):
+                    os.replace(os.getcwd() + '\\' + file_name,
+                               os.getcwd() + '\\' + folder_name
+                               + '\\' + file_name)
+
+if __name__ == '__main__':
+    main()
+    input("Sorting is finished.")
