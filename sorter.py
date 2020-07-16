36f05d67ca28330ab9639575612c2fdb3dfb2bbb
Demian Ruban <48176400+demianruban@users.noreply.github.com>
2020-07-16 19:35:56 +0300

    updated

--- sorter.py
+++ sorter.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from tkinter improt Tk, messagebox
 
 main_dict = {
     'Executables': ('.exe',),
@@ -10,22 +11,20 @@ main_dict = {
     'Videos'     : ('.mp4', '.AVI', '.mkv')
     }
 
-def main():
-    
-    p = Path(os.getcwd())
-    
-    for folder_name, file_formats in main_dict.items():
-        
-        if not os.path.exists(folder_name): os.mkdir(folder_name)
+root = Tk()
+root.withdraw()
+if not messagebox.askyesno("Ask", "Are you sure"): exit()
 
-        for file_format in file_formats:
-            for file in list(map(str, list(p.glob('*' + file_format)))):
-                source = p / file
-                destination = p / folder_name / file
-                os.replace(str(source.cwd()), str(destination.cwd()))
+p = Path(os.getcwd())
 
-                
-if __name__ == '__main__':
-    print("Current location:", os.getcwd())
-    main()
-    input("Sorting is finished.")
+for folder_name, file_formats in main_dict.items():
+    
+    if not os.path.exists(folder_name): os.mkdir(folder_name)
+
+    for file_format in file_formats:
+        for file in list(map(str, list(p.glob('*' + file_format)))):
+            source = p / file
+            destination = p / folder_name / file
+            os.replace(str(source.cwd()), str(destination.cwd()))
+            
+messagebox.showinfo("Info", "Sorting is finished.")
