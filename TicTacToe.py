f114dc99dde902befccde0b4e2b4cdbb875c1950
Demian Ruban <48176400+demianruban@users.noreply.github.com>
2020-07-28 13:21:57 +0300

    update tic tac toe
    still does not work

--- TicTacToe.py
+++ TicTacToe.py
@@ -6,31 +6,35 @@
 # There will no position writing needed anymore
 # Moves are made with special keys on keyboard.
 
+from random import randint
+
 def get_winner(board):
     wins = ((0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6))
-    for index1, index2, index3 in wins:
-        pos = [board[index1], board[index2], board[index3]]
-        pos[0] if all(e == pos[0] for e in pos) else False
+    for i, j, k in wins:
+        pos = [board[i], board[j], board[k]]
+        return pos[0] if all(e == pos[0] for e in pos) else False
 
 def get_opponent_for(player):
     return 'X' if player=='O' else 'O'
     
-def minimax(board, player) -> int:
-    return  bool(get_winner(board))
-        
+def minimax(board, player, mode) -> int:
+    
+    return 1 if player == get_winner(board) else -1
+    
     move = -1
     score = -2
 
-    for i in range(9):
-        if board[i] == '_':
-            board_with_new_move = board
-            board_with_new_move[i] = player
-            score_for_the_move = -minimax(board_with_new_move,
-                                          get_oponnent_for(player))
-            if score_for_the_move > score:
-                score = score_for_the_move
-                move = i
-
+    if mode == 1:
+        for i in range(9):
+            if board[i] == '_':
+                board_with_new_move = board
+                board_with_new_move[i] = player
+                score_for_the_move = -minimax(board_with_new_move,
+                                              get_oponnent_for(player))
+                if score_for_the_move > score:
+                    score = score_for_the_move
+                    move = i
+    
     if move == -1:
         return 0
 
@@ -49,6 +53,7 @@ def move(board, player):
         if 'q' in user_input:
             exit()
         elif user_input in moves and board[moves[user_input]] == '_':
+            print() # make line
             board[moves[user_input]] = player
             break
         else:
@@ -56,7 +61,16 @@ def move(board, player):
             continue
     return
 
-def start():
+
+def draw_board(board):
+    indexes = [0, 1, 2]
+    for i in range(3):
+        print(' ' + ' | '.join(board[j] for j in indexes))
+        if i < 2: print('---+---+---')
+        indexes = [j + 3 for j in indexes]
+    print()
+
+def ask_mode():
     while True:        
         a = input("Choose game mode:\n"
                   + "1. Two players.\n"
@@ -67,33 +81,22 @@ def start():
             print("Wrong input. Try again.")
             continue
 
+# Script
 
-def draw_board():
-    indexes = [0, 1, 2]
-    for i in range(3):
-        print(' ' + ' | '.join(board[j] for j in indexes))
-        if i < 2: print('---+---+---')
-        indexes = [j + 3 for j in indexes]
-    print()
+board = ['_' for i in range(9)]
 
+player = 'X' if randint(0, 1) == 0 else 'O'
 
-def main():
-    board = ['_' for i in range(9)]
-    player1, player2, computer = '', '', ''
-    mode = 0
+mode = ask_mode()
 
-    while True:
-        mode = start()
-        if mode == 1:
-            player1, player2 = 'X', 'O'
-        else:
-            computer, player1 = 'X', 'O'
-        draw_board()
-        move(board, player)
-
-        print(get_winner(board))
-
-if __name__ == '__main__':
-    main()
-
-        
+while True:
+    player = get_opponent_for(player)
+    draw_board(board)
+    move(board, player)
+    result = minimax(board, player, mode)
+    if result == 1:
+        input(player + " won!")
+        break
+    elif result == 0:
+        input("Draw.")
+        break
