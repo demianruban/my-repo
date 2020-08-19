2ec58946ba930c92fc9b1ec4d952cf27f4e27178
Demian Ruban <48176400+demianruban@users.noreply.github.com>
2020-08-19 13:42:57 +0300

    updated

--- game/cars.py
+++ game/cars.py
@@ -1,31 +1,43 @@
 from random import choice as random_choice
 import pygame
 pygame.init()
-pygame.time.set_timer(pygame.USEREVENT, 1500)
+pygame.time.set_timer(pygame.USEREVENT, 1000)
 
+###########################
+# Sounds.
 boom = pygame.mixer.Sound('boom.wav')
+obema = pygame.mixer.music.load('chelovek.mp3') # 4 seconds
 
+# Constants.
 W = 400
 H = 400
-GREY = (100, 100, 100)
+GREY =  (100, 100, 100)
+WHITE = (255, 255, 255)
+GREEN = (  0, 255,   0)
 CARS_SURF = []
 
 sc = pygame.display.set_mode((W, H))
- 
+
+# List of car images (variants).
 for i in ('car1.png', 'car2.png', 'car3.png'):
     CARS_SURF.append(pygame.image.load(i).convert_alpha())
 
-CAR_W = CARS_SURF[0].get_width()
+car_w = CARS_SURF[0].get_width()
 
-
+# Paths on which cars will drive
 ROAD_LINES = []
 x = W
-for i in range(W // CAR_W):
-    x -= CAR_W
+for i in range(W // car_w):
+    x -= car_w
     ROAD_LINES.append(x)
+
+del x, car_w
+
+###########################
 
 
 class Car(pygame.sprite.Sprite):
+    """Class that represents each NPC car on screen."""
     def __init__(self, x, surf, group):
         pygame.sprite.Sprite.__init__(self)
         self.image = surf
@@ -34,27 +46,37 @@ class Car(pygame.sprite.Sprite):
         self.speed = random_choice((2, 2.5, 3))
  
     def update(self):
+        pygame.draw.rect(sc, (0, 255, 0), self.rect, 1)
         if self.rect.y < H:
             self.rect.y += self.speed
         else:
             self.kill()
+
+def test():
+    pygame.draw.rect(sc, GREEN, rect, 1)
+    for npc_car in cars:
+        pygame.draw.rect(sc, GREEN, npc_car.rect, 1)
  
 cars = pygame.sprite.Group()
 
-cars_on_screen = [Car(random_choice(ROAD_LINES), random_choice(CARS_SURF), cars)]
+Car(random_choice(ROAD_LINES), random_choice(CARS_SURF), cars)
 
 car = pygame.image.load('car.png')
 rect = car.get_rect(center=(W // 2, H - car.get_width()))
 rot = car.copy()
+
+font = pygame.font.Font(None, 50)
+game_over = font.render("EBLAN", 0, WHITE,)
+text_rect = game_over.get_rect(center=(200, 200))
 
 while 1:
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
             exit()
         elif i.type == pygame.USEREVENT:
-            cars_on_screen.append(
-            Car(random_choice(ROAD_LINES), random_choice(CARS_SURF), cars))
+            Car(random_choice(ROAD_LINES), random_choice(CARS_SURF), cars)
 
+    # Player controls.
     keys = pygame.key.get_pressed()
     if keys[pygame.K_UP]:
         rot = car.copy()
@@ -69,16 +91,26 @@ while 1:
         rot = pygame.transform.rotate(car, 90)
         rect.centerx -= 3
 
-    for npc_car in cars_on_screen:
+    # Conditions of defeat.
+    for npc_car in cars:
         if rect.colliderect(npc_car):
+            
             boom.play()
-            sc.blit(pygame.font.Font(None, 50).render("Eblan", 0, (255, 255, 255)), (200, 200))
+            pygame.time.delay(500)
+            pygame.mixer.music.play(start=2)
+            sc.blit(game_over, text_rect)
             pygame.display.update()
-            pygame.time.wait(1500)
-            exit()
+            while True:
+                for i in pygame.event.get():
+                    if i.type == pygame.QUIT: exit()
+            
  
     sc.fill(GREY)
+    rect = rot.get_rect(center=rect.center)
     sc.blit(rot, rect)
+    ###########
+    test()
+    ###########    
     cars.draw(sc)
     pygame.display.update()
     pygame.time.delay(20)
